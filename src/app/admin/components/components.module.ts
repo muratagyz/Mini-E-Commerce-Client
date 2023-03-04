0304f54dab3ddc064c475fa4f5eee2f2cc88f139
MURAT AĞYÜZ <182804010@ogr.cbu.edu.tr>
2023-03-04 15:17:22 +0300

    04.03.2023

---
+++ src/app/admin/components/components.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { CustomersModule } from './customers/customers.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductsModule,
+    OrdersModule,
+    CustomersModule,
+    DashboardModule
+  ],
+})
+export class ComponentsModule {}
---
+++ src/app/ui/components/components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsModule } from './products/products.module';
+import { BasketsModule } from './baskets/baskets.module';
+import { HomeModule } from './home/home.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, BasketsModule, HomeModule],
+})
+export class ComponentsModule {}
